18237f0aa9ab7a81c31b80ae894be90acf77d833
sven <svenarne.lindstroem@gmail.com>
2024-02-15 16:46:59 +0100

    updated ai skeliton

--- Pig/ai.py
+++ Pig/ai.py
@@ -1,7 +1,28 @@
 class Ai():
     def __init__(self, difficulty) -> None:
-        pass
+        self.name = ""        
+        self.behaviour = self.select_dificulty(difficulty) 
 
     def roll_dice(self) -> bool:
-        return True:
+        return self.behaviour():
         
+    def select_dificulty(self, difficulty) -> function:
+        def simple(game) -> bool:
+            pass
+        def normal(game) -> bool:
+            pass
+        def hard(game) -> bool:
+            pass
+
+        match difficulty:
+            case 1:
+                self.name = "pig-let"
+                return simple
+            case 2:
+                self.name = "pig"
+                return normal
+            case 3:
+                self.name = "Boar"
+                return hard
+
+
