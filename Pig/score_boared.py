3ce83cfc0c899c78f868e97b61d4b68f1a088a41
sven <svenarne.lindstroem@gmail.com>
2024-02-17 19:30:06 +0100

    added score boared tests, more needed

--- Pig/score_boared.py
+++ Pig/score_boared.py
@@ -5,32 +5,37 @@ class ScoreBoared():
     
     def __init__(self) -> None:
         players = dict()
-        self.loade_scores()
+        file_name = "high_scores.bin"
+        self.loade_scores(file_name)
     
     def up_date_score(self, game) -> None:
-        self.up_date_game_played(game.player1)
-        self.up_date_game_played(game.player2)
-        self.up_date_game_won(game.current_player)
+        self.up_date_game_played(game.player1.name)
+        self.up_date_game_played(game.player2.name)
+        self.up_date_game_won(game.current_player.name)
 
     def up_date_game_played(self, player) -> None:
-        self.players[player.name] = self.players.get(player.name, {"wins":0, "played":0})
-        self.players[player.name]["played"] += 1
+        self.players[player] = self.players.get(player, {"wins":0, "played":0})
+        self.players[player]["played"] += 1
         
     def up_date_game_won(self, player) -> None:
-        self.players[player.name]["wins"] += 1
+        self.players[player] = self.players.get(player, {"wins":0, "played":0})
+        self.players[player]["wins"] += 1
 
     def up_date_name(self, old_name, new_name) -> None:
         self.players[new_name] = self.players[old_name]
         del self.players[old_name]
 
+    def name_exists(self, name):
+        return name in self.players
+
     def save_scores(self) -> None:
         with open("high_scores.bin", "wb") as f:
-            pikle.dump(self.players, f)
+            pickle.dump(self.players, f)
 
-    def load_scores(self) -> None:
+    def load_scores(self, file_name) -> None:
         try:
-            with open("high_scores.bin", "rb") as f:
-                self.players = pikle.load(f)
+            with open(file_name, "rb") as f:
+                self.players = pickle.load(f)
         except FileNotFoundError:
             print("cring Error no scores useing empty score board")
             
---
+++ test/test_score_boared.py
@@ -0,0 +1,38 @@
+import unittest
+from Pig import score_boared
+
+class test_score_boared(unittest.TestCase):
+
+    def test_init(self):
+        res = score_boared.ScoreBoared()
+        exp = score_boared.ScoreBoared
+        self.assertIsInstance(res, exp)
+
+    def test_up_date_score(self):
+        boared = score_boared.ScoreBoared()
+    
+    def test_up_date_played(self):
+        boared = score_boared.ScoreBoared()
+        boared.up_date_game_played('test_name')
+        self.assertEqual(1 == boared.players['test_name']["played"])
+    
+    def test_up_date_won(self):
+        boared = score_boared.ScoreBoared()
+        boared.up_date_game_won('test_name')
+        self.assertEqual(1 == boared.players['test_name']["wins"])
+ 
+    def test_up_date_name(self):
+        boared = score_boared.ScoreBoared()
+        boared.up_date_game_played('test_name')
+        boared.up_date_name('test_name', 'new_name')
+        self.assertFalse('test_name' in boared.players)
+        self.assertTrue('new_name' in boared.players)
+    
+    def test_name_exists(self):
+        boared = score_boared.ScoreBoared()
+        boared.up_date_game_played("test_name")
+        self.assertTrue(boared.name_exists("test_name"))
+        self.assertFalse(boared.name_exists("test"))
+
+
+ 
