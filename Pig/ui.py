46d417947c558698cd5cca7c22cc55c943194d4d
Marta Moroni <marta.moroni7@gmail.com>
2024-02-19 09:15:52 +0100

    Correct commit

--- Pig/ui.py
+++ Pig/ui.py
@@ -1,18 +1,94 @@
+# change things from cmd to normal user input 
+
 import cmd
 
 class Ui(cmd.Cmd):
-    prompt = "(Game) > "
+    def __init__(self) -> None:
+        self.prompt = "(Game) > "
+        self.completekey = "tab"
+        self.cmdqueue = []
 
-    # menu to show when it starts running
+        self.in_menu = False
+        self.in_start = False
+        self.in_difficulty = False
 
-    # menu after the start
-    # plus start game
+        self.initialization_menu()
 
-    # menu if choses AI
 
-    def do_quit(self, argv):
+    def initialization_menu(self):
+        print("\nWelcome to PIG!")
+        self.do_MENU(self)
+
+
+    def do_MENU(self, argv):
+        print("\n----- MENU -----")
+        print("1. Start") # New game
+        print("2. ContinueGame")
+        print("3. Board")
+        print("4. ChangeName")
+        print("5. Rules")
+        print("6. Exit\n")
+        self.in_menu = True
+
+    
+    def do_Start(self, argv):
+        if self.in_menu:
+            print("\nChoose game type:\n")
+            print("1. 1VS1")
+            print("2. 1VSAI")
+            print("MENU")
+            self.in_menu = False
+            self.in_start = True
+        else:
+            print("Select option from current menu")
+
+
+    def do_1VS1(self, argv):
+        pass
+
+
+    def do_1VSAI(self, argv):
+        if self.in_start:
+            print("\nChoose difficulty:\n")
+            print("1. Piglet")
+            print("2. Pig")
+            print("3. Boar")
+            print("4. MENU")
+            self.in_start = False
+            self.in_start = True
+
+    
+    def do_Piglet(self, argv):
+        pass
+
+
+    def do_Pig(self, argv):
+        pass
+
+
+    def do_Boar(self, argv):
+        pass
+
+
+    def do_ContinueGame(self, argv):
+        pass
+
+    def do_Board(self, argv):
+        pass
+
+    def do_ChangeName(self, argv):
+        pass
+
+    def do_Rules(self, argv):
+        pass
+
+    def do_Exit(self, argv):
         """This exits the program"""
-        return True
+        if self.in_menu:
+            return True
+        else:
+            print("Select option from current menu")
+    
     
     #aliasing
-    do_exit = do_quit
+    do_menu = do_MENU
