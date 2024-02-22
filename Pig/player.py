4da69ee9c84148f46d525713facf9ba7aa794875
Marta Moroni <marta.moroni7@gmail.com>
2024-02-22 15:00:42 +0100

    player class implemented

--- Pig/player.py
+++ Pig/player.py
@@ -3,4 +3,18 @@ class Player():
         self.name = name
 
     def roll_dice(self) -> bool:
-        return True
+        valid_choice = False
+
+        while not valid_choice:
+            choice = input("Do you want to roll the dice? (yes/no)").lower()
+
+            if choice == "yes":
+                return True
+            elif choice == "no":
+                return False
+            else:
+                print("Please enter valid choice: 'yes' or 'no'")
+
+    def change_name(self):
+        new_name = input("Set new name: ")
+        self.name = new_name
