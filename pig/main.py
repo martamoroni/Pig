2650a865585b9587d5164a8b558902f993c6121e
SvenLindstrom <svenarne.Lindstroem@gmail.com>
2024-03-07 16:51:21 +0100

    now path is tottaly fixed trust me bro

--- pig/main.py
+++ pig/main.py
@@ -1,10 +1,17 @@
 """Module that defines the main class."""
 
+import os
+import sys
+
+dir = os.path.dirname(os.path.abspath(__file__))
+proj_dir = os.path.abspath(os.path.join(dir, ".."))
+sys.path.append(proj_dir)
+
+
 import ui
-
-
 def main():
     """Start user interface."""
+    
     ui2 = ui.Ui()
     ui2.cmdloop()
 
